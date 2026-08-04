ef27bb9e013b116d6e6777ef1d20fa0401778dbe
weilycoder <weily2009@outlook.com>
2026-08-04 19:58:32 +0800

    feat: Enhance argument handling and logging level support

--- force-remove.cpp
+++ force-remove.cpp
@@ -1,12 +1,28 @@
 #include "force-remove.hpp"
 
 int main(int argc, char *argv[]) {
-  Logger logger(Logger::debug_v);
-  if (argc < 2) {
-    logger.error("Usage: force-remove <path>");
+  if (argc < 2 || argc > 3) {
+    std::cout << "Usage: force-remove <path> [<log_level>]" << std::endl;
     return -1;
   }
+
   std::string path = argv[1];
+  int log_level = Logger::info_v;
+
+  if (argc == 3) {
+    try {
+      log_level = std::stoi(argv[2]);
+      if (log_level < Logger::debug_v || log_level > Logger::error_v) throw std::out_of_range("");
+    } catch (const std::invalid_argument &) {
+      std::cerr << "Invalid log level: " << argv[2] << std::endl;
+      return -1;
+    } catch (const std::out_of_range &) {
+      std::cerr << "Log level out of range: " << argv[2] << std::endl;
+      return -1;
+    }
+  }
+
+  Logger logger(log_level);
   ForceRemove(path, logger);
   return logger.getMsgFlag() & (1u << Logger::error_v) ? 1 : 0;
 }
