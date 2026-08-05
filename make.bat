7ea9130e06825e69c3a79e8e806af459f5c240d1
weilycoder <weily2009@outlook.com>
2026-08-05 14:50:37 +0800

    build: Modify make.bat to build forcedelete.exe with UPX compression

--- make.bat
+++ make.bat
@@ -1,1 +1,3 @@
-@g++ -o forcedelete.exe force-remove.cpp nt.cpp utils.cpp -lpathcch -static -O3
+@echo off
+g++ -o forcedelete.exe force-remove.cpp nt.cpp utils.cpp -lpathcch -static -O3
+upx --best --ultra-brute forcedelete.exe
